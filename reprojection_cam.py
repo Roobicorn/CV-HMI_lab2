b3ca818e7316f26f61d6fcb573362fd7a28944c6
Reuben Hunter <63873721+Roobicorn@users.noreply.github.com>
2024-12-19 14:13:54 +0000

    before calibration attempt 2

---
+++ reprojection_cam.py
@@ -0,0 +1,155 @@
+import cv2
+import numpy as np
+import csv
+
+#Create VideoCapture instance
+cap = cv2.VideoCapture(0)
+#Check if device can be opened, exit if not
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+#Set the resolution for image capture
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+
+#Initialise an empty list of images and the number to be captured
+number_of_images = 10
+#imglist = []
+success = True
+
+#Initialise empty list for chessboard corner points in world and image co-ordinates
+#objpoints = []
+imgpoints = []
+#retlist = []
+
+#Initialise chessboard object dimensions #8mm when using phone, 25mm for printed sheet
+objp = np.zeros((9*6, 3), np.float32)
+objp[:,:2] = 8*np.mgrid[0:6, 0:9].T.reshape(-1,2)
+#print(objp)
+
+#define function to draw cartesian axes
+def draw(img, originpts, imgpts):
+    origin = tuple(originpts[0].ravel().astype(int))
+    img = cv2.line(img, origin, tuple(imgpts[0].ravel().astype(int)), (255,0,0), 5)
+    img = cv2.line(img, origin, tuple(imgpts[1].ravel().astype(int)), (0,255,0), 5)
+    img = cv2.line(img, origin, tuple(imgpts[2].ravel().astype(int)), (0,0,255), 5)
+    return img
+
+#axis matrix: 3 orthogonal vectors
+axis = np.float32([[30,0,0], [0,30,0], [0,0,-30]]).reshape(-1,3)
+
+#import camera calibration parameters
+mtx = np.genfromtxt("intrinsic_matrix.csv", delimiter=",")
+dist = np.genfromtxt("distortion_coefficients.csv", delimiter=",")
+
+############ Comment out this block to avoid repeating capture ################
+#Capture images from video
+#Set up windows for the video stream and captured images
+cv2.namedWindow("Captured Image")
+cv2.namedWindow("Video Stream")
+
+#Loop through the indices of images to be captured
+capcount = 0
+for imgnum in range(number_of_images):
+    #Capture images continuously and wait for a keypress
+    while success and cv2.waitKey(1) == -1:
+        #Read an image from the Videocapture instance
+        success, img = cap.read()
+        
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, imgpoints = cv2.findChessboardCorners(gray, (9,6), cv2.CALIB_CB_ADAPTIVE_THRESH + 
+            cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+        
+        if ret == True:
+            img = cv2.drawChessboardCorners(img, (9,6), imgpoints, ret)
+            ret, rvecs, tvecs = cv2.solvePnP(objp, imgpoints, mtx, dist)
+            projpoints, jacobian = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+            img = draw(img, imgpoints, projpoints)
+
+        #Display the image
+        cv2.imshow("Video Stream", img)
+   
+    #When we exit the capture loop we save the last image and repeat
+    cv2.imwrite("ProjectedImage%03d.png" % (imgnum), img)
+    capcount += 1
+    cv2.imshow("Captured Image", img)
+    print("Image captured")
+
+#The Image index loop ends when number_of_images habe been captured
+print("Captured", capcount, "images")
+
+#Clean up the viewing window and release the VideoCapture instance
+cv2.destroyWindow("Captured Image")
+cv2.destroyWindow("Video Stream")
+cap.release() 
+#####################################################################
+
+# #Read images from files:
+# for imgnum in range(number_of_images):
+#     img = cv2.imread("Image%03d.png" % imgnum, cv2.IMREAD_GRAYSCALE)
+
+#     ret, corners = cv2.findChessboardCorners(img, (9,6), cv2.CALIB_CB_ADAPTIVE_THRESH + 
+#         cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+#     if ret == True:
+#         #img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+#         imglist.append(img)
+#         objpoints.append(objp)
+#         imgpoints.append(corners)
+#         retlist.append(ret)
+#         print("Image%03d.png keypoints captured" % imgnum)
+#     else:
+#         print("Keypoints not detected in Image%03d.png" % imgnum)
+
+# print("Keypoints captured for", len(imglist), "images")
+
+
+# #Refine localisation of detected 3D points:
+# #print("pre refining:", imgpoints[0][0], imgpoints[5][0]) #for testing
+# for imgnum in range(len(imglist)):
+#     subcorners = cv2.cornerSubPix(imglist[imgnum], imgpoints[imgnum],(11,11), (-1,-1), 
+#         (cv2.TermCriteria_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+#     imgpoints[imgnum] = subcorners
+#     print("subcorners calculated for image", imgnum)
+# #print("post refining:", imgpoints[0][0], imgpoints[5][0])
+
+
+# #calibrate camera based on data collected
+# print("Calibrating...")
+# ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[0:2],
+#     None, (cv2.TERM_CRITERIA_COUNT+cv2.TERM_CRITERIA_EPS, 30, 0.0001))
+
+# #output and save calibration data
+# print("\nIntrinsic Matrix: \n")
+# print(mtx)
+# print("\nDistortion Coefficients: \n")
+# print(dist)
+# print("\nRotation Vectors:\n")
+# print(rvecs)
+# print("\nTranslation Vectors:\n")
+# print(tvecs)
+
+# with open("intrinsic_matrix.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(mtx)
+
+# with open("distortion_coefficients.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(dist)
+
+# with open("imgpoints.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(imgpoints)
+
+# with open("objpoints.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(objpoints)
+
+# with open("rotation_vectors.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(rvecs)
+
+# with open("translation_vectors.csv", "w") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(tvecs)
---
+++ reprojection_img.py
@@ -0,0 +1,113 @@
+import cv2
+import numpy as np
+import csv
+
+#Create VideoCapture instance
+cap = cv2.VideoCapture(0)
+#Check if device can be opened, exit if not
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+#Set the resolution for image capture
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+
+#Initialise an empty list of images and the number to be captured
+number_of_images = 50
+#imglist = []
+success = True
+
+#Initialise empty list for chessboard corner points in world and image co-ordinates
+#objpoints = []
+#imgpoints = []
+#retlist = []
+
+#Initialise chessboard object dimensions #8mm when using phone, 25mm for printed sheet
+objp = np.zeros((9*6, 3), np.float32)
+objp[:,:2] = 8*np.mgrid[0:6, 0:9].T.reshape(-1,2)
+#print(objp)
+
+#define function to draw cartesian axes
+def draw(img, originpts, imgpts):
+    origin = tuple(originpts[0].ravel().astype(int))
+    img = cv2.line(img, origin, tuple(imgpts[0].ravel().astype(int)), (255,0,0), 5)
+    img = cv2.line(img, origin, tuple(imgpts[1].ravel().astype(int)), (0,255,0), 5)
+    img = cv2.line(img, origin, tuple(imgpts[2].ravel().astype(int)), (0,0,255), 5)
+    return img
+
+#axis matrix: 3 orthogonal vectors
+axis = np.float32([[30,0,0], [0,30,0], [0,0,-30]]).reshape(-1,3)
+
+
+#import camera calibration parameters
+mtx = np.genfromtxt("intrinsic_matrix.csv", delimiter=",")
+dist = np.genfromtxt("distortion_coefficients.csv", delimiter=",")
+
+############ Comment out this block to avoid repeating capture ################
+# #Capture images from video
+# #Set up windows for the video stream and captured images
+# cv2.namedWindow("Captured Image")
+# cv2.namedWindow("Video Stream")
+
+# #Loop through the indices of images to be captured
+# capcount = 0
+# for imgnum in range(number_of_images):
+#     #Capture images continuously and wait for a keypress
+#     while success and cv2.waitKey(1) == -1:
+#         #Read an image from the Videocapture instance
+#         success, img = cap.read()
+        
+#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         ret, imgpoints = cv2.findChessboardCorners(gray, (9,6), cv2.CALIB_CB_ADAPTIVE_THRESH + 
+#             cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+        
+#         if ret == True:
+#             img = cv2.drawChessboardCorners(img, (9,6), imgpoints, ret)
+#             ret, rvecs, tvecs = cv2.solvePnP(objp, imgpoints, mtx, dist)
+#             projpoints, jacobian = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+#             img = draw(img, imgpoints, projpoints)
+
+#         #Display the image
+#         cv2.imshow("Video Stream", img)
+   
+#     #When we exit the capture loop we save the last image and repeat
+#     cv2.imwrite("ProjectedImage%03d.png" % (imgnum), img)
+#     capcount += 1
+#     cv2.imshow("Captured Image", img)
+#     print("Image captured")
+
+# #The Image index loop ends when number_of_images habe been captured
+# print("Captured", capcount, "images")
+
+# #Clean up the viewing window and release the VideoCapture instance
+# cv2.destroyWindow("Captured Image")
+# cv2.destroyWindow("Video Stream")
+# cap.release() 
+#####################################################################
+
+#setup display window
+cv2.namedWindow("Projected Image")
+
+#Read images from files:
+for imgnum in range(number_of_images):
+    #img = cv2.imread("Image%03d.png" % imgnum)
+    img = cv2.imread("calibrated_undistortion_crop.png")
+
+    ret, imgpoints = cv2.findChessboardCorners(img, (9,6), cv2.CALIB_CB_ADAPTIVE_THRESH + 
+        cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+    if ret == True:
+        img = cv2.drawChessboardCorners(img, (9,6), imgpoints, ret)
+        ret, rvecs, tvecs = cv2.solvePnP(objp, imgpoints, mtx, dist)
+        projpoints, jacobian = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        img = draw(img, imgpoints, projpoints)
+
+        cv2.imshow("Projected Image", img)
+        cv2.waitKey(0)
+        
+        cv2.imwrite("ReprojectedImage%03d.png" % imgnum, img)
+
+        print("ReprojectedImage%03d.png captured" % imgnum)
+    else:
+        print("Chessboard not detected in Image%03d.png" % imgnum)
